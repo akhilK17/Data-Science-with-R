b51da68b2801a9d87f3cce197551808492faa352
akhilK17 <vaidh.akhil@gmail.com>
2018-08-29 22:35:37 +0530

    Added the script for pie chart

--- PlotsCategoricalVar.R
+++ PlotsCategoricalVar.R
@@ -2,7 +2,7 @@
 
 require("datasets") 	# attach package datasets in the working environment
 ?chickwts		# help/information can be taken from the documents
-data("chickwts")	# load the chickwts data in working enviornment
+data("chickwts")	# load the chickwts data in working environment
 str(chickwts)		# dataset structure can be obtained by str function
 plot(chickwts$feed)	# plot function will plot the feed frequency
 
@@ -16,7 +16,7 @@ par(oma = c(1, 1, 1, 1))  # sets outside margins of barplot
 par(mar = c(4, 5, 2, 1))  # Sets plots margins
 
 # Many other arguments can be used to make visually sound barplot
-# Some are used below to visualze this in good manner
+# Some are used below to visualize this in good manner
 
 barplot(feeds[order(feeds)],
         horiz  = TRUE,
@@ -26,5 +26,17 @@ barplot(feeds[order(feeds)],
         main   = "Frequencies on Different Feeds\nin chickwts Dataset",  # \n is line break
         xlab   = "Number of Chicks"
         )
+
+# Pie Charts for categorical variable
+
+pie(feeds)		# a simple pie plot of the feeds
+?pie			# "?" is to get help from documents
+
+# modifying the pie chart to make it interesting and colourful to visualize in a better way
+pie(feeds[order(feeds, decreasing = TRUE)], 
+    init.angle = 90,  # starts at 12 o'clock instead of 3
+    clockwise = TRUE,
+    col = rainbow(6),
+    main = "Pie Chart of Feeds from chickwts")
 
 rm(list = ls())		# to clear out the working environment
